a2588bcc5a904d990c8807fc146c232ff321198e
lastinm <lastinm@mail.ru>
2024-05-03 00:15:46 +0300

    dvc new datasets

--- lab4/src/ohe_to_dataset.py
+++ lab4/src/ohe_to_dataset.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Прочитаем датасет из файлов CSV
-train_df = pd.read_csv('../dataset/train.csv')
-test_df = pd.read_csv('../dataset/test.csv')
+train_df = pd.read_csv('../datasets/train.csv')
+test_df = pd.read_csv('../datasets/test.csv')
 
 print("Информация об исходном датасете:")
 print(train_df.info())
@@ -19,8 +19,8 @@ print(train_df.info())
 
 try:
     # Сохранение обновленных датасетов в CSV
-    train_df.to_csv('../dataset/train.csv', index=False)
-    test_df.to_csv('../dataset/test.csv', index=False)
+    train_df.to_csv('../datasets/train.csv', index=False)
+    test_df.to_csv('../datasets/test.csv', index=False)
     print("Датасеты успешно сохранены в каталоге ../dataset под именами train.csv и test.csv.")
 except Exception as e:
     print("Произошла ошибка при сохранении датасетов:", e)
